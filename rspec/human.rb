e9c6c96ae4f3b4132147f12e70ffaba527eba532
mogulla3 <hs.iarn3@gmail.com>
2018-03-22 11:37:24 +0900

    fixed

--- rspec/human.rb
+++ rspec/human.rb
@@ -7,16 +7,11 @@ class Human
     "I'm going to eat #{dish}"
   end
 
-  # def study(subject, hour)
-  #   "I'm going to study #{subject} for #{hour} hour"
-  # end
-
   def sleep
-    "a"
-  #   "I'm going to sleep"
-  #   sleeping_hours = (1..12).to_a.sample
-  #   raise RuntimeError if sleeping_hours < 3
-  # rescue RuntimeError
-  #   "I couldn't sleep well"
+    "I'm going to sleep"
+    sleeping_hours = (1..12).to_a.sample
+    raise RuntimeError if sleeping_hours < 3
+  rescue RuntimeError
+    "I couldn't sleep well"
   end
 end
